05232cba482e6fd2686c7ce6d4948ebbac04ac03
fithamlak <justiceofgod01@gmail.com>
2023-04-28 14:41:29 +0300

    Define solver class and Add #factorial test befor logic

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,12 @@
 require 'rspec'
+require './solver'
 
 describe Solver do
-
+  describe '#factorial' do
+    it 'takes an argument' do
+      s_obj = Solver.new
+      arg = 0
+      Solver.factorial(arg)
+    end
+  end
 end
