df3e3cda861126854b126dfb04aaebd23ef384c4
fithamlak <justiceofgod01@gmail.com>
2023-04-28 15:04:46 +0300

    Add spec for factorial to raises an exception if the argument is negative

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,7 +3,7 @@ require './solver'
 
 describe Solver do
   describe '#factorial' do
-    it 'takes an integer as an argument' do
+    it 'takes 0 as an argument and return 1' do
       # Arrange
       s_obj = Solver.new
       arg = 0
@@ -11,6 +11,14 @@ describe Solver do
       result = s_obj.factorial(arg)
       #assert
       expect(result).to eq(1)
+    end
+
+    it 'raises an exception if the argument is negative' do
+      # Arrange
+      s.obj = Solver.new
+      arg = -1
+      # Assert
+      expect { s_obj.factorial(arg) }.to raise_error(ArgumentError)
     end
   end
 end
