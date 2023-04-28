375e9d4aa328f88b208e6779b24fc712fcbe6bb3
Mirna <ampueromirna8@gmail.com>
2023-04-28 07:44:07 -0500

    put reverse in solver

--- solver.rb
+++ solver.rb
@@ -3,5 +3,9 @@ class Solver
     return 1 if arg.zero?
     raise ArgumentError, "#{self.class} '#{__method__}' argument cannot be negative" if arg.negative?
   end
+  
+  def reverse(string) 
+
+  end 
 
 end
